968aa91f652af9b93dc412386c65f2827170b245
plantvsbirds <plantvsbird@gmail.com>
2017-04-18 19:43:56 -0500

    files + parenthesis

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ Promise.timeout = (time) => new Promise((res) => {
 })
 
 const token = '288807406:AAEoxNGZLWnuBCalkcddEI4tJY7Y9-QcDE8',
-      bot   = new Telebot({token, polling : {}})
+      bot   = new Telebot({token, polling : {}}),
+      PandasGroup = -178851802,
+      Self = 59886156
 
 let data = JSON.parse(fs.readFileSync('data.json', {encoding : 'utf8'}))
 const userExists = (name) => !!data.users.filter((u) => u.name == name).length
@@ -19,46 +21,92 @@ bot.on('/start', ({text, from}) => {
   return bot.sendMessage(id, 'Hi')
 })
 
+const accessToFile = (msg) => {
+  return msg.chat.id == PandasGroup || msg.from.username == 'hipvb'
+}
+
 import Time from './commands/time'
 bot.on('/time', Time(bot))
 import Poll from './commands/poll'
 bot.on('/poll', Poll(bot))
 
-bot.on('/stash', (msg) => {
-  let {text, from, chat} = msg
-  let { id } = chat
-  let stash = text.split(' ').slice(1).join('-')
-  const u = getUser(msg)
-  data.files[stash] = u.files.slice(0)
-  u.files = []
-  return bot.sendMessage(id, `已归档${data.files[stash].length}条至 ${stash}，权限为公开`)
-})
-
-bot.on('/pop', ({text, from, chat}) => {
-  let { id } = chat
-  let stash = text.split(' ').slice(1).join(' ')
-
-  if (data.files[stash] && data.files[stash].length) {
-    let msgs = data.files[stash].map((msg) => bot.forwardMessage(id, msg.chat.id, msg.message_id, {notify : true}))
-    return Promise.seq(msgs.map(p => {
-      return function () {
-        return p
-      }
-    }))
-      .then(Promise.timeout(1000))
-      .then(bot.sendMessage(id, '以上。'))
-  } else {
-    return bot.sendMessage(id, '归档空。')
+bot.on('*', (msg) => {
+  let {
+    message_id,
+    from,
+    chat,
+    date,
+    text,
+    reply_to_message,
+    document,
+    audio,
+    video
+  } = msg
+  if (document && accessToFile(msg)) {
+    data.files.push(msg)
+    return bot.sendMessage(chat.id, `File ${document.file_name} saved.`)
   }
 })
 
-bot.on('*', msg => {
-  if (!msg.forward_from)
-    return
-  const u = getUser(msg)
-  u.files.push(msg)
-  return bot.sendMessage(msg.from.id, '已推入缓冲区')
+;['mp4', 'pdf', 'docx'].forEach(fileType => {
+  bot.on(`/${fileType}`, (msg) => {
+    let {
+      message_id,
+      from,
+      chat,
+      date,
+      text,
+      reply_to_message,
+      document,
+      audio,
+      video
+    } = msg
+    if (!accessToFile(msg))
+      return bot.sendMessage(chat.id, 'Access Denied.')
+    let firstFile = data.files.filter(f => f.document && f.document.file_name.endsWith(`.${fileType}`))[0]
+    if (firstFile)
+      return bot.forwardMessage(chat.id, firstFile.chat.id, firstFile.message_id)
+    else
+      return bot.sendMessage(chat.id, `I found no .${fileType} file.`)
+  })
+  bot.on(`/${fileType}s`, (msg) => {
+    const maxNumber = 5
+    let {
+      message_id,
+      from,
+      chat,
+      date,
+      text,
+      reply_to_message,
+      document,
+      audio,
+      video
+    } = msg
+    if (!accessToFile(msg))
+      return bot.sendMessage(chat.id, 'Access Denied.')
+    let files = data.files.filter(f => f.document && f.document.file_name.endsWith(`.${fileType}`)).slice(0,maxNumber)
+    console.log(files)
+    if (files)
+      return Promise.all(files.map(f => {
+        return bot.forwardMessage(chat.id, f.chat.id, f.message_id)
+      }).concat([
+        bot.sendMessage(chat.id, `${Math.min(maxNumber, files.length)} file(s) shown (${maxNumber} max)`)
+      ]))
+    else
+      return bot.sendMessage(chat.id, `I found no .${fileType} file.`)
+  })
 })
+
+const parenthesisFillFuncGenerator = (left, right) => ({text, from, chat, message_id}) => {
+  let llen = text.split(left).length - 1,
+      rlen = text.split(right).length - 1
+  if (llen - rlen)
+    return bot.sendMessage(chat.id, Array(llen - rlen).fill(right).join(''), {reply : message_id, notify : false})
+}
+
+bot.on('text', parenthesisFillFuncGenerator('（','）'))
+bot.on('text', parenthesisFillFuncGenerator('(',')'))
+bot.on('text', parenthesisFillFuncGenerator('[',']'))
 
 bot.connect()
 
